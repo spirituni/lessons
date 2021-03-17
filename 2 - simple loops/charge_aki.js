4c4699a975895c3508310ba73bda84854c33fe82
Judy Chen <jubnaut@gmail.com>
2021-03-16 23:27:01 -0700

    questions and answers

--- 2 - simple loops/charge_aki.js
+++ 2 - simple loops/charge_aki.js
@@ -4,7 +4,9 @@
 var battery = 50;       // perecentage of battery power that aki has (0 - 100)
 
 var indicator = '';     // a battery indicator message
-var bar = '';           // a battery indicator bar
+var bar = '';           // a battery indicator bar made of boxes
+
+const maxLength = 10;   // maximum number of boxes in the battery bar (read-only)
 
 function updateBar() {
   
@@ -29,9 +31,10 @@ function updateBar() {
   var length = 0;
 
   while (length < 10) {
-      bar += '▯';                // can also be written: bar = bar + '▯';
-      ++length;                   // can also be written: length = length + 1;
-                                  // or even            : length += 1;
+    bar += '▯';                // can also be written: bar = bar + '▯';
+    ++length;                   // can also be written: length = length + 1;
+                                // or even            : length += 1;
+                                // or                 : length++;
   }
 
 // 1 ii
@@ -40,14 +43,14 @@ function updateBar() {
 
   // filled boxes
   while (length < battery/10) {   // can also be written: while (length*10 < battery) {
-      bar += '▮';
-      ++length;
+    bar += '▮';
+    ++length;
   }
 
   // empty boxes
   while (length < 10) {
-      bar += '▯';
-      ++length;
+    bar += '▯';
+    ++length;
   }
 
 // 1 iii
@@ -55,12 +58,12 @@ function updateBar() {
   var length = 0;
 
   while (length < 10) {
-      if (length < battery/10) {
-        bar += '▮';
-      } else {
-        bar += '▯';
-      }
-      ++length;
+    if (length < battery/10) {
+      bar += '▮';
+    } else {
+      bar += '▯';
+    }
+    ++length;
   }
 
 /*
@@ -68,21 +71,55 @@ function updateBar() {
 
     2. let's try to make all of those again, but with for loops instead of while loops!
 
+      i. using a for loop, make a bar full of 10 empty boxes
+
+      ii. using two for loops, have the bar show filled boxes for every 10% of battery that aki has
+
+      iii. now do it using one for loop and an if block (if/else/else if statements)!
 */
 
-// 2
+// 2 i
+  bar = '';
 
-  // for (let i = 0; i < battery; i += 10) {
-  //   bar += '▮';
-  // }
+  for (var i = 0; i < 10; ++i) {
+    bar += '▯';
+  }
 
-  // const pips = bar.padEnd(10, '▯');
-  // bar = pips;
+// 2 ii
+  bar = '';
 
+  // filled boxes
+  var i = 0;                      // can also be written as: var i;
+  for (; i < battery/10; ++i) {   //                         for (i = 0; i < battery/10; ++i) {
+    bar += '▮';
+  }
+
+  // empty boxes
+  for (; i < 10; ++i) {
+    bar += '▯';
+  }
+
+  // 2 iii
+  bar = '';
+
+  var i = 0;
+  for (var i = 0; i < 10; ++i) {
+    if (i < battery/10) {
+      bar += '▮';
+    } else {
+      bar += '▯';
+    }
+  }
+  
 /*
-  (bonus) cleaning it all up:
-    3. go back and remove or comment out all the code that no longer does anything!
+  (bonus) constants:
+    3. instead of writing out the length of the bar each time, try using a constant!
+    
+    4. now do all the loops using decrements (--) instead of increments (++), but make sure to keep the battery bar behaviour the same
 
 */
+
+  // 3
+    // replace every 10 in your code above with maxLength
 
 }
