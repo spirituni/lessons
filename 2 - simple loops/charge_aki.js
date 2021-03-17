6c5eece2dfabc318bbf0848b88154a437130f5a7
Judy Chen <jubnaut@gmail.com>
2021-03-16 23:29:05 -0700

    remove answers

--- 2 - simple loops/charge_aki.js
+++ 2 - simple loops/charge_aki.js
@@ -27,44 +27,16 @@ function updateBar() {
 */
 
 // 1 i
-  bar = '';
-  var length = 0;
 
-  while (length < 10) {
-    bar += '▯';                // can also be written: bar = bar + '▯';
-    ++length;                   // can also be written: length = length + 1;
-                                // or even            : length += 1;
-                                // or                 : length++;
-  }
+
 
 // 1 ii
-  bar = '';
-  var length = 0;
 
-  // filled boxes
-  while (length < battery/10) {   // can also be written: while (length*10 < battery) {
-    bar += '▮';
-    ++length;
-  }
 
-  // empty boxes
-  while (length < 10) {
-    bar += '▯';
-    ++length;
-  }
 
 // 1 iii
-  bar = '';
-  var length = 0;
 
-  while (length < 10) {
-    if (length < battery/10) {
-      bar += '▮';
-    } else {
-      bar += '▯';
-    }
-    ++length;
-  }
+
 
 /*
   for loops:
@@ -79,37 +51,12 @@ function updateBar() {
 */
 
 // 2 i
-  bar = '';
 
-  for (var i = 0; i < 10; ++i) {
-    bar += '▯';
-  }
+
 
 // 2 ii
-  bar = '';
 
-  // filled boxes
-  var i = 0;                      // can also be written as: var i;
-  for (; i < battery/10; ++i) {   //                         for (i = 0; i < battery/10; ++i) {
-    bar += '▮';
-  }
 
-  // empty boxes
-  for (; i < 10; ++i) {
-    bar += '▯';
-  }
-
-  // 2 iii
-  bar = '';
-
-  var i = 0;
-  for (var i = 0; i < 10; ++i) {
-    if (i < battery/10) {
-      bar += '▮';
-    } else {
-      bar += '▯';
-    }
-  }
   
 /*
   (bonus) constants:
@@ -120,6 +67,7 @@ function updateBar() {
 */
 
   // 3
-    // replace every 10 in your code above with maxLength
+
+  
 
 }
