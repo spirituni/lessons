d148bc3d881bded63245fc481e3a39ce2e0d77f1
Judy Chen <jubnaut@gmail.com>
2021-04-06 01:31:49 -0700

    using loops

--- 4 - arrays/scripts/scripts.js
+++ 4 - arrays/scripts/scripts.js
@@ -1,26 +1,60 @@
-var i = 'x';
+var p = 'x';
+var message = 'wins';
 
 window.onload = function() {
   restart();
+  message = document.getElementById('message');
 };
 
 function restart() {
-  i = 'x';
+  // reset player
+  p = 'x';
+  message.innerHTML = 'next player: ' + p;
+  winner = '';
 
+  // reset buttons
   document.querySelectorAll('button.grid').forEach(b => {
     b.disabled = false;
     b.innerHTML = '';
   });
+
+  // reset boards
+  board = ['-', '-', '-', '-', '-', '-', '-', '-', '-'];
+  board2d = [
+    ['-', '-', '-'],
+    ['-', '-', '-'],
+    ['-', '-', '-']
+  ];
 }
 
 function clicked(b) {
   b.disabled = true;
-  b.innerHTML = i;
-  grid[b.id] = i;
+  b.innerHTML = p;
 
-  if (i == 'x') {
-    i = 'o';
+  let i = Math.floor(b.id/3);
+  let j = b.id - i*3;
+  board[b.id] = p;
+  board2d[i][j] = p;
+  // console.log(b.id, i, j);
+
+  check(p, b.id);
+  check2d(p, i, j);
+
+  // swap players
+  if (p == 'x') {
+    p = 'o';
   } else {
-    i = 'x';
+    p = 'x';
+  }
+
+  if (winner != '') {
+    message.innerHTML = winner + " wins!";
+
+    document.querySelectorAll('button.grid').forEach(b => {
+      b.disabled = true;
+    });
+
+  } else {
+    message.innerHTML = 'next player: ' + p;
   }
 }
--- 4 - arrays/tictakitoe.js
+++ 4 - arrays/tictakitoe.js
@@ -1,15 +1,166 @@
-// tic tac toe grid
+/* 
+  tic tac toe board
+*/
 
-var grid = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', '']
+// 1d array
+var board = ['-', '-', '-', '-', '-', '-', '-', '-', '-'];
+
+// 2d array
+var board2d = [
+  ['-', '-', '-'],
+  ['-', '-', '-'],
+  ['-', '-', '-']
 ];
 
-function check() {
+/*
+  other variables
+*/
+
+var winner = '';      // winner of the game ('x', 'o', or '')
+
+
+function check(player, i) {
+
+  // player: the letter that was just played ('x' or 'o')
+  // i:      the index where the player just placed their letter
 
   /*
+
+    setup:
+
+      1. before we start, let's print out the array using a loop
+
+      2. now that we have the value of each array slot printing out, let's format it in a way that's easy to read
+
+  */
+
+  var out = '';
+  for (var x = 0; x < 9; ++x) {
+    out += board[x];
+
+    if ((x+1)%3 == 0) {
+      out += '\n';
+    }
+  }
+  console.log(out);
+
+  /*
+
+    win conditions:
+
+      3. check whether the player wins horizontally (within a row)
+
+      4. check whether the player wins vertically (within a column)
+
+      5. check whether the player wins diagonally
 
   */
 
 }
+
+function check2d(player, row, col) {
+
+  // player: the letter that was just played ('x' or 'o')
+  // row:    the number of the row that the player just placed their letter in 
+  // col:    the number of the column that the player just placed their letter in 
+
+  /*
+  
+    setup:
+
+      1. same as before, let's print out the array, only this time, we can to use a loop within a loop (nested loops)
+
+
+           0     1     2     (j)
+
+      0    0,0   0,1   0,2
+
+
+      1    1,0   1,1   1,2
+
+
+      2    2,0   2,1   2,2
+
+     (i)
+
+
+  */
+
+  var out = '';
+  for (var i = 0; i < 3; ++i) {
+    for (var j = 0; j < 3; ++j) {
+      out += board2d[i][j];
+    }
+    out += '\n';
+  }
+  console.log(out);
+
+  /*
+
+    win conditions:
+
+      2. check whether the player wins horizontally (within a row)
+
+      3. check whether the player wins vertically (within a column)
+
+      4. check whether the player wins diagonally
+
+  */
+
+  /*
+    with just conditions
+  */
+
+
+  /*
+
+    with loops
+
+  */
+
+  // 2
+  var win = true;
+  for (var j = 0; j < 3; ++j) {
+    if (board2d[row][j] != player) {
+      win = false;
+    }
+  }
+
+  if (win) {
+    winner = player;
+  } else {
+    // 3
+    win = true;
+    for (var i = 0; i < 3; ++i) {
+      if (board2d[i][col] != player) {
+        win = false;
+      }
+    }
+
+    if (win) {
+      winner = player;
+    } else {
+      // 4
+      win = true;
+      if (board2d[1][1] != player) {
+        win = false;
+      } else {
+        if (!(board2d[0][0] == player && board2d[2][2] == player) && !(board2d[0][2] == player && board2d[2][0] == player)) {
+          win = false;
+        }
+        if (win) {
+          winner = player;
+        }
+      }
+
+    }
+  }
+  // console.log(row, col)
+  // console.log(win);
+}
+
+/*
+
+  bonus: what are some ways you can optimize this? think about checks that you're making that might not need to be made in certain situations (hint - can you win with less than 3 letters?)
+
+*/
